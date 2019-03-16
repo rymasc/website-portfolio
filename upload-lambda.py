33db5a95d6dc1cb7fc2c87a70655f0b03a10f6d5
Ryan Scott <ryan.matt.scott@gmail.com>
2019-03-16 19:58:08 -0400

    add sns topic to notify successful build

--- upload-lambda.py
+++ upload-lambda.py
@@ -5,20 +5,27 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
     
-    website_bucket = s3.Bucket('ryanmattscott.com')
-    build_bucket = s3.Bucket('build.ryanmattscott.com')
+    sns = boto3.resource('sns') 
+    topic = sns.Topic('arn:aws:sns:us-east-1:366391210774:deployRMSWebsiteTopic')
     
-    build_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('websitebuild.zip', build_zip)
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+        website_bucket = s3.Bucket('ryanmattscott.com')
+        build_bucket = s3.Bucket('build.ryanmattscott.com')
     
-    with zipfile.ZipFile(build_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            website_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            website_bucket.Object(nm).Acl().put(ACL='public-read')
+        build_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('websitebuild.zip', build_zip)
     
-    return 'Hello from Lambda'
+        with zipfile.ZipFile(build_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                website_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                website_bucket.Object(nm).Acl().put(ACL='public-read')
+    
+        topic.publish(Subject='Publish Lambda', Message='Website successfully Deployed')
+    except:
+        topic.publish(Subject='Publish Lambda', Message="Website deployment error")
+        raise
+    return 'hi from Lambda'
