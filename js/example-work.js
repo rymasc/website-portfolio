a984d77b5c195c42cdcc6fc7c7da112a35a9ac52
Ryan Scott <ryan.matt.scott@gmail.com>
2019-03-17 01:17:26 -0400

    Add react component for work example bubbles

---
+++ js/example-work.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+class ExampleWork extends React.Component {
+  render() {
+    return (
+      <section class="section section--alignCentered section--description">
+        {this.props.work.map((example, i) => {
+          return <ExampleWorkBubble example={example} key={i} />;
+        })}
+      </section>
+    );
+  }
+}
+
+class ExampleWorkBubble extends React.Component {
+  render() {
+    let example = this.props.example;
+    return (
+      <div class="section__exampleWrapper">
+        <div class="section__example">
+          <img
+            alt={example.image.desc}
+            class="section__exampleImage"
+            src={example.image.src}
+          />
+          <dl class="color--cloud">
+            <dt class="section__exampleTitle section__text--centered">
+              {example.title}
+            </dt>
+            <dd />
+          </dl>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ExampleWork;
--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
-import "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import ExampleWork from "./example-work";
 
-console.log("Loaded react-dom");
-console.log("test");
+const myWork = [
+  {
+    title: "Work Example",
+    image: {
+      desc: "example screenshot of a project involving code",
+      src: "images/example1.png",
+      comment: ""
+    }
+  },
+
+  {
+    title: "Work Example",
+    image: {
+      desc: "example screenshot of a project involving chemistry",
+      src: "images/example2.png",
+      comment: ""
+    }
+  },
+  {
+    title: "Work Example",
+    image: {
+      desc: "example screenshot of a project involving cats",
+      src: "images/example3.png",
+      comment: ""
+    }
+  }
+];
+ReactDOM.render(
+  <ExampleWork work={myWork} />,
+  document.getElementById("example-work")
+);
